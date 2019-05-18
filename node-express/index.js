6e3cd0b04234c4db23d1bab9b4ed529e2c23584e
belalElkady <belal.elkady4@gmail.com>
2019-05-18 18:50:41 +0200

    Morgan example

--- node-express/index.js
+++ node-express/index.js
@@ -5,14 +5,10 @@ const hostname='localhost';
 const port=3000;
 
 const app=express();
+app.use(morgan('dev'));
 
-app.use((req,res,next)=>{
+app.use(express.static(__dirname + '/public'));
 
-    res.statusCode=200;
-    res.setHeader('Content-Type','text/html');
-    res.end('<html><body><h1>Express Server</h1></body></html>')
-
-});
 
 const server = http.createServer(app);
 
